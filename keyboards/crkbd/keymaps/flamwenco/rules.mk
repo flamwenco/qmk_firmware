0cf2cbe298f1d382f6053f1b9bf18a63103f5091
Daniel Muckerman <danielmuckerman@me.com>
2021-06-29 11:32:31 -0400

    update manta keymap

---
+++ keyboards/crkbd/keymaps/flamwenco/rules.mk
@@ -0,0 +1,12 @@
+
+# Build Options
+#   change to "no" to disable the options, or define them in the Makefile in
+#   the appropriate keymap folder that will get included automatically
+#
+RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
+VIA_ENABLE = yes
+OLED_DRIVER_ENABLE = yes
+LTO_ENABLE = yes
+WPM_ENABLE = yes
+# RAW_ENABLE = yes            # Enable RAW HID communication to PC
+EXTRAFLAGS += -flto
